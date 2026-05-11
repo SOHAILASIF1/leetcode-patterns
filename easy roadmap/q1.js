d05778db403925f41e0618aadee769c21dd5d33c
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-12 00:03:53 +0500

    auto commit

--- easy roadmap/q1.js
+++ easy roadmap/q1.js
@@ -38,3 +38,16 @@
 
 //Soulation
 //brute force
+let nums=[1,1,1,3,3,4,3,2,4,2]
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < array.length; j++) {
+        if (arr[i]===arr[j]) {
+            return true
+            
+        }
+
+        
+        
+    }
+}
+return false
