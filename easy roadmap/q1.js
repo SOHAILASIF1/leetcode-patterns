bada318015c42fbab29d08bd272ffcfcfcc6c769
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-12 00:05:33 +0500

    auto commit

--- easy roadmap/q1.js
+++ easy roadmap/q1.js
@@ -42,7 +42,8 @@ let nums=[1,1,1,3,3,4,3,2,4,2]
 for (let i = 0; i < nums.length; i++) {
     for (let j = i+1; j < nums.length; j++) {
         if (nums[i]===nums[j]) {
-            return true
+            console.log(true);
+            
             
         }
 
@@ -50,4 +51,5 @@ for (let i = 0; i < nums.length; i++) {
         
     }
 }
-return false
+console.log(false);
+
