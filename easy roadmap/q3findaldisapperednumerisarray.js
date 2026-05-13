d8448e95d5c851739a61af5b8b203deca1397829
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-13 15:44:14 +0500

    auto commit

--- easy roadmap/q3findaldisapperednumerisarray.js
+++ easy roadmap/q3findaldisapperednumerisarray.js
@@ -31,7 +31,7 @@ for (let i = 1; i < nums.length; i++) {
         
     }
     if (nums[i] !== nums[i - 1] + 1) {
-        temp.push(arr[i - 1] + 1)
+        temp.push(nums[i - 1] + 1)
         
         
     }
