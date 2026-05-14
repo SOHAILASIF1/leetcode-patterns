91d27483b3b41a8321347439e040ba2e3a544356
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-14 23:15:19 +0500

    auto commit

--- easy roadmap/q4missingnumber.js
+++ easy roadmap/q4missingnumber.js
@@ -24,7 +24,7 @@
 
 // Example 3:
 
-// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Input: 
 
 // Output: 8
 
@@ -35,18 +35,38 @@
  
 
 //first approch math approch
-let arr=[9,6,4,2,3,5,7,0,1]
 
-let sum=0
+// let nums = [9,6,4,2,3,5,7,0,1]
+// var missingNumber = function(nums) {
+//     let sum = 0
+    
+//     nums.forEach(num => {
+//         sum += num
+//     })
+    
+//     let n = nums.length
+//     let expectedSum = (n * (n + 1)) / 2
+    
+//     return expectedSum - sum
+// }
+// console.log(missingNumber(nums));
 
-for (let i = 0; i < arr.length; i++) {
-    sum+=arr[i]
+
+//hashing approch
+// let nums = [9,6,4,2,3,5,7,0,1]
+let nums = [9,6,4,2,3,5,7,0,1]
+
+function missingNumber(nums) {
+    let set=new Set(nums)
+    for (let i = 0; i < nums.length; i++) {
+        const element = array[i];
+        
+    }
     
     
 }
-let n = arr.length
-let expectedSum = (n * (n + 1)) / 2
-let missingNumber = expectedSum - sum
+console.log(set);
 
-console.log(missingNumber) // 8
+
+
 
