e524a08003389cd3734070afd07584ee3c5b2485
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-14 23:42:44 +0500

    auto commit

--- easy roadmap/q4missingnumber.js
+++ easy roadmap/q4missingnumber.js
@@ -82,7 +82,7 @@ let nums=[9,6,4,2,3,5,7,0,1]
 
 nums.sort((a,b)=>a-b)
 for (let i = 0; i <=nums.length; i++) {
-    if (i!=arr[i]) {
+    if (i!=nums[i]) {
         console.log(i);
         
         
