f649d9e152c133ffe6a06aead0a50b998ef94344
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-19 07:22:36 +0500

    auto commit

--- easy roadmap/q5majorityelement.js
+++ easy roadmap/q5majorityelement.js
@@ -47,10 +47,10 @@ let nums = [2,2,1,3,3,3,3,1,2,3,3,3,3,2,3,3];
 
 //hash map approch
 let map = new Map()
-let count=1
+
 
 for (let i = 0; i < nums.length; i++) {
-    
+    let count=1
     
     if (map.has(nums[i])) {
         count ++
